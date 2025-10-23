158ad9a22c11b3ffc8b59785919c1b60ddf775b5
Eleve <joas.sautrel@gmail.com>
2025-10-23 10:06:10 +0200

    bonjour

---
+++ bataille_navale/src/Touche_coule.java
@@ -0,0 +1,25 @@
+public class Touche_coule {
+public static void main(String[]args){
+
+    int n, i, j;
+    int[][]Tab = new int [10][10];
+
+    while (Tab[i,j]!=0){
+        n=Tab[i,j]
+    }
+    if (n!=0){
+        //s'il a touché un nb différent de à, un bateau a été touché, sinon le coup est parti dans l'eau !
+        //variable = Tab[i,j] (et comparer à 0), et qd une case est touchée, changer numéro en -1 pr dire qu'elle a déjà été touchée (afficher msg d'erreur si le joueur retouche la mm case)
+        // bat=touche;
+
+        System.out.println("Un bateau a été touché");
+    //S'il ne reste plus de mm chiffre (ex : 4-4-4-4),le bateau est touché coulé.
+    for (i=0;i<=9; i++){
+
+    }
+    }
+    else {
+        System.out.println("Le coup est parti dans l'eau");
+    }
+}
+}
